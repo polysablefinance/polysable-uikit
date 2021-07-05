ff65b71836d3ff9471a89e5c02d5c8275bc9aefb
polysablefinance <polysable@protonmail.com>
2021-07-04 23:44:12 -0700

    a

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2819,7 +2819,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#eda185", backgroundDisabled: "#dde6da", contrast: "#b08a66", invertedContrast: "#f1faee", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#6e3b6e", textDisabled: "#a4a9ab", textSubtle: "#6e3b6e", borderColor: "#8C342B", card: "#b08a66", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#857a48", background: "#8e95e8", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#191326", input: "#eeeaf4", primaryDark: "#427bf5", tertiary: "#f7e9dc", text: "#857a48", textDisabled: "#BDC2C4", textSubtle: "#857a481", borderColor: "#524B63", card: "#427bf5", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#81A594", background: "#8e95e8", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#191326", input: "#eeeaf4", primaryDark: "#00628B", tertiary: "#f7e9dc", text: "#857a48", textDisabled: "#BDC2C4", textSubtle: "#81A594", borderColor: "#524B63", card: "#00628B", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
